170c3042732d3c440b815b660816b15c11a7e305
Rudyb3 <Rudy.bevernage@gmail.com>
2025-05-01 14:15:22 +0200

    yay

--- Configure.sh
+++ Configure.sh
@@ -1,14 +1,35 @@
+#!/bin/bash
+clear
+
 # -------------------------------- 
 # Configure script
 # --------------------------------
 
+# Variables
+
+downloadFolder="$HOME/.riceing"
+
+# Functions
+
+if [ ! -d $download_folder ]; then
+    mkdir -p $download_folder
+fi
+
+_installYay() {
+    _installPackages "base-devel"
+    SCRIPT=$(realpath "$0")
+    temp_path=$(dirname "$SCRIPT")
+    git clone https://aur.archlinux.org/yay.git $download_folder/yay
+    cd $download_folder/yay
+    makepkg -si
+    cd $temp_path
+    echo ":: yay has been installed successfully."
+}
+
 # Prepare Environment
 
-pacman -S --needed git base-devel --noconfirm && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sri
+_installYay
 
-# Download Config
-git clone https://github.com/rudyb3/hyprland
-
-cd hyprland
+#cd hyprland
 
 cp .config ~/.config -rf
